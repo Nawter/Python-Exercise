991d6ffc7f65b32165d204c679074ebb8e854ebf
zaiddawoodissa <zaiddawoodissa@gmail.com>
2017-04-22 15:19:34 +0100

    added CharacterInput to repo

---
+++ CharacterInput.py
@@ -0,0 +1,5 @@
+# Character Input using python 3
+name = input("What is your name my friend: ")
+age = int(input("How old are you my dear: "))
+year = str((2017 - age) + 100)
+print(name + " will be 100 years old in the year " + year)
